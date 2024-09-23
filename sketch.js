409c24857b758c0dbb4ea46f404413f3c85f4310
Cassidy Williams <clw23d@fsu.edu>
2024-09-23 00:13:53 -0400

    added the basics

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+let diameter = 20;
+
+
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER);
+
+  strokeWeight(5);
+
+  gui = createGui();
+  diameterSlider = createSlider("diameter",200,300,100,30,10,200)
 }
 
 function draw() {
   background(125);
+  drawGui();
+
+  fill(255);
+  circle(100,100,diameterSlider.val);
+
+  text("graphic design is my passion",40,100)
+
+  text(diameterSlider.label, 200,350)
+
 }
